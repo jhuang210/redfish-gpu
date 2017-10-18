0f38ed2aa4036f01a37d5eafd359cef3e85fb75a
jhuang210 <jiahui.huang210@gmail.com>
2017-10-17 17:10:42 -0700

    add multi-threading feature

--- redfish-gpu/smc-redfish-gpu.py
+++ redfish-gpu/smc-redfish-gpu.py
@@ -6,6 +6,10 @@ import requests
 import re
 import threading
 import time
+import urllib3
+import Queue
+
+urllib3.disable_warnings()
 
 class SMCRedfish:
     def __init__(self, host="", user="ADMIN", pwd="ADMIN"):
@@ -37,10 +41,29 @@ class SMCRedfish:
 class SMCRedfishGPU:
     def __init__(self):
         self.rf_obj=None
+        self.body=None
     def setRedfishObj(self, obj):
         self.rf_obj=obj
     def getRedfishObj(self):
         return self.rf_obj
+    def getFormattedSEL(self):
+        self.rf_obj.redfish_get("/Chassis/1/Thermal")
+        self.data=self.rf_obj.getBodyDict()
+        ret_val=""
+        for item in self.data['Temperatures']:
+            name=item['Name']
+            regexp=re.compile(r'GPU[0-9] Temp')
+            if regexp.search(name):
+                #sys.stdout.write(item['MemberID']+':')
+                #ret_val=ret_val+item['MemberID']+'|'
+                ret_val=ret_val+item['Name'].split()[0]+':'+ str(item['ReadingCelsius'])+'|'
+        return ret_val
+    def getFormattedSEL_Thread(self, queue):
+        output=""
+        gpu_ids=self.getFormattedSEL()
+        ip_str=self.getRedfishObj().getHost().split('/')[-1]
+        output="Thermal(%-14s): %s \n" % (ip_str,gpu_ids)
+        queue.put(output)
 class SMCPostCodeSnoop:
     def __init__(self):
         self.rf_obj=None
@@ -81,14 +104,69 @@ class SMCGPUMonitor:
             obj=SMCRedfishGPU()
             obj2=SMCPostCodeSnoop()
             rf=SMCRedfish(smc_sut[x])
-            rf.redfish_login()
+            #rf.redfish_login()
             obj.setRedfishObj(rf)
             obj2.setRedfishObj(rf)
             self.gpu_obj.append(obj)
             self.post_obj.append(obj2)
-    def on_finish(self):
+    def on_login(self):
+        #for obj in self.gpu_obj:
+        #    obj.getRedfishObj().redfish_login()
+        tmp_threads=[]
         for obj in self.gpu_obj:
-            self.obj.logout()
+            rf_obj=obj.getRedfishObj()
+            new_thread=threading.Thread(target=rf_obj.redfish_login, name='Thread-'+rf_obj.getHost())
+            tmp_threads.append(new_thread)
+            new_thread.start()
+        for t in tmp_threads:
+            t.join()
+    def on_logout(self):
+        #for obj in self.gpu_obj:
+        #    obj.getRedfishObj().redfish_logout()
+        tmp_threads=[]
+        for obj in self.gpu_obj:
+            rf_obj=obj.getRedfishObj()
+            new_thread=threading.Thread(target=rf_obj.redfish_logout, name='Thread-'+rf_obj.getHost())
+            tmp_threads.append(new_thread)
+            new_thread.start()
+        for t in tmp_threads:
+            t.join()
+    def on_monitor(self):
+        #output=[]
+        #try:
+        #    for obj in self.gpu_obj:
+        #        gpu_ids=obj.getFormattedSEL()
+        #        ip_str=obj.getRedfishObj().getHost().split('/')[-1]
+        #        output.append("Thermal(%-14s): %s \n" % (ip_str,gpu_ids))
+        #    for i in output:
+        #        sys.stdout.write(i)
+        #    sys.stdout.flush()
+            #for j in range(0,len(output)):
+            #    sys.stdout.write("\033[F")
+        #    sys.stdout.write('\n')
+        #    sys.stdout.flush()
+        #    time.sleep(1)
+        #except(KeyboardInterrupt,SystemExit):
+        #    print("Pressed Ctrl+C")
+        #    self.state=-1            
+        tmp_threads=[]
+        output_queue=Queue.PriorityQueue()
+        try:
+            for obj in self.gpu_obj:
+                new_thread=threading.Thread(target=obj.getFormattedSEL_Thread, args=[output_queue])
+                tmp_threads.append(new_thread)
+                new_thread.start()
+            for t in tmp_threads:
+                t.join()
+            while not output_queue.empty():
+                sys.stdout.write(output_queue.get())
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+            time.sleep(1)
+        except(KeyboardInterrupt,SystemExit):
+            print("Pressed Ctrl+C")
+            self.state=-1
+        sys.stdout.flush()
     def on_execute(self):
         while(self._running):
             if(self.state==0):
@@ -104,39 +182,41 @@ class SMCGPUMonitor:
                     sys.stdout.write("Timeout, exit\n")
                     sys.stdout.flush()
                     self.state=-1
-                #for obj in self.gpu_obj:
-                #    ret=self.check_network(obj.getRedfishObj().getHost())
+                for obj in self.gpu_obj:
+                    ret=self.check_network(obj.getRedfishObj().getHost())
             elif(self.state==2):
                 #State#3: Login
-#                self.gpu_obj.login()
+                self.on_login()
                 self.state=3
             elif(self.state==3):
                 #State#4: Start monitoring
-                output=[]
-                try:
-                    for obj in self.post_obj:
-                        postcode=obj.getCurPostcode()
-                        ip_str=obj.getRedfishObj().getHost()
-                        #sys.stdout.write("Postcode(%s): %s \n" % (ip_str,postcode))
-                        output.append("Postcode(%s): %s \n" % (ip_str,postcode))
-                    for i in output:
-                        sys.stdout.write(i)
-                    sys.stdout.flush()
-                    for j in range(0,len(output)):
-                        sys.stdout.write("\033[F")
-                    sys.stdout.flush()
-                    time.sleep(1)
-                except(KeyboardInterrupt,SystemExit):
-                    print("Pressed Ctrl+C")
-                    self.state=-1
+                #output=[]
+                #try:
+                #    for obj in self.post_obj:
+                #        postcode=obj.getCurPostcode()
+                #        ip_str=obj.getRedfishObj().getHost()
+                #        #sys.stdout.write("Postcode(%s): %s \n" % (ip_str,postcode))
+                #        output.append("Postcode(%s): %s \n" % (ip_str,postcode))
+                #    for i in output:
+                #        sys.stdout.write(i)
+                #    sys.stdout.flush()
+                #    for j in range(0,len(output)):
+                #        sys.stdout.write("\033[F")
+                #    sys.stdout.flush()
+                #    time.sleep(1)
+                #except(KeyboardInterrupt,SystemExit):
+                #    print("Pressed Ctrl+C")
+                #    self.state=-1
+                ######################################
+                self.on_monitor()
             elif(self.state==-1):
                 #State#End: Exiting
                 self._running=False
-#                self.on_finish()
+                self.on_logout()
             else:
                 #print("Unexpected State: Exit")
                 print "Unexpected State: Exit"
-                self._running=False
+                self._running=False     
 if __name__=="__main__":
     GPUMoniter=SMCGPUMonitor()
     GPUMoniter.on_execute()
